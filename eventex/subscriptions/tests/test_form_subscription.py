2a03515cb2043bee6e96ab453ace53158508c0b5
adriano <adriano@debian>
2017-06-23 07:19:15 -0300

    Email ou telefone opcionais

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -20,10 +20,26 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='8521')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
-    def test_must_to_be_captalized(self):
+    def test_name_must_to_be_captalized(self):
         """Name must to be captalized"""
         form = self.make_validated_form(name="ADRIANO oliveira")
         self.assertEqual('Adriano Oliveira', form.cleaned_data['name'])
+
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email or phone are optional, bust one must be informed"""
+        form = self.make_validated_form(email='', phone='')
+        """__all__ is error of the form"""
+        self.assertListEqual(['__all__'], list(form.errors))
 
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -79,3 +79,11 @@ class SubscriptionsNewPostInvalid(TestCase):
 
     def test_save_dont_subscription(self):
         self.assertFalse(Subscription.objects.exists())
+
+
+class TemplateRegressionTest(TestCase):
+    def test_template_has_no_field_errors(self):
+        invalid_data = dict(name='Adriano Oliveira', cpf='85215856320')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+        
+        self.assertContains(response, '<ul class="errorlist nonfield">')
