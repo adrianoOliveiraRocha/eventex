e8eca4852f7d23783454366f0d513760df848186
adriano <adriano@debian>
2017-06-26 08:07:36 -0300

    Simplifica com modelform

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from eventex.subscriptions.validators import validate_cpf
 
 
 class Subscription(models.Model):
     name = models.CharField(max_length=100, verbose_name='nome')
-    cpf = models.CharField(max_length=11, verbose_name='CPF')
-    email = models.EmailField(verbose_name='e-mail')
-    phone = models.CharField(max_length=20, verbose_name='telefpne')
+    cpf = models.CharField(max_length=11, verbose_name='CPF',
+                           validators=[validate_cpf])
+    email = models.EmailField(verbose_name='e-mail', blank=True)
+    phone = models.CharField(max_length=20, verbose_name='telefone',
+                             blank=True)
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='criado em')
     paid = models.BooleanField(default=False, verbose_name='pago')
